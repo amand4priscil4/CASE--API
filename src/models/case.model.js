f5a117a48d6c5266c83ae0f87e741a6a8738002b
amand4priscil4 <amanda.priscilaa15@gmail.com>
2025-04-26 09:18:41 -0300

    Update case.model.js

--- src/models/case.model.js
+++ src/models/case.model.js
@@ -1,19 +1,52 @@
 const mongoose = require('mongoose');
 
 const caseSchema = new mongoose.Schema({
-  titulo: { type: String, required: true },
-  descricao: { type: String, required: true },
-  data: { type: Date, default: Date.now },
+  titulo: {
+    type: String,
+    required: true
+  },
+  tipo: {
+  type: String,
+  enum: [
+    'acidente',
+    'identificação de vítima',
+    'exame criminal',
+    'exumação',
+    'violência doméstica',
+    'avaliação de idade',
+    'avaliação de lesões',
+    'avaliação de danos corporais'
+  ],
+  required: true
+},
+  descricao: {
+    type: String,
+    required: true
+  },
+  data: {
+    type: Date,
+    default: Date.now
+  },
   status: {
     type: String,
-    enum: ['aberto', 'em andamento', 'concluído'],
-    default: 'aberto'
+    enum: ['em andamento', 'finalizado', 'arquivado'],
+    default: 'em andamento'
   },
   peritoResponsavel: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true
   },
-  localDoCaso: { type: String, required: true }
-});
+  localDoCaso: {
+    type: String,
+    required: true
+  },
+  criadoPor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, { timestamps: true });
+
 module.exports = mongoose.model('Case', caseSchema);
+
