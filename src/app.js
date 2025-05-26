738e9ea9063598f5bde1771996e6063c4dc4496f
amand4priscil4 <amanda.priscilaa15@gmail.com>
2025-05-26 00:39:02 +0000

    update frontend configuration

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors'); // Importado o middleware cors
+const cors = require('cors');
 const app = express();
-const uri = "mongodb+srv://amandapriscilaa15:S99rV4gzO6u9MYaw@cluster0.sd5eblw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"; // Substitua isso!
+
+// Configuração do MongoDB
+const uri = "mongodb+srv://amandapriscilaa15:S99rV4gzO6u9MYaw@cluster0.sd5eblw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 mongoose
   .connect(uri, {
@@ -16,8 +18,12 @@ mongoose
 app.use(
   cors({
     origin: [
+      'http://127.0.0.1:5500', 
+      'http://localhost:3000',
+      'http://localhost:5173',
       'https://sistema-olin.netlify.app',
-      'https://opulent-capybara-5gw9g7p49q734xwj-5173.app.github.dev'
+      'https://opulent-capybara-5gw9g7p49q734xwj-5173.app.github.dev',
+      /^https:\/\/.*\.app\.github\.dev$/ // Aceita qualquer Codespace
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
@@ -31,26 +37,27 @@ app.use(express.json());
 // Importa a função createAdmin para executá-la ao iniciar
 require('./create/createAdmin');
 
-// Rotas
+// Importações das rotas
 const authRoutes = require('./routes/auth.routes'); // Login, geração de token
-const protectedRoutes = require('./routes/protected.routes'); // Rotas protegidas genéricas (tipo /api/protegido)
+const protectedRoutes = require('./routes/protected.routes'); // Rotas protegidas genéricas
 const userRoutes = require('./routes/user.routes'); // Cadastro, update e listagem de usuários
 const caseRoutes = require('./routes/case.routes'); // Gerenciamento de casos (CRUD)
 const evidenceRoutes = require('./routes/evidence.routes'); // Gerenciamento de evidências
 const laudoRoutes = require('./routes/laudo.routes'); // Gerenciamento de laudos
-const relatorioRoutes = require('./routes/relatorio.routes'); // criação do relatório final
+const relatorioRoutes = require('./routes/relatorio.routes'); // Criação do relatório final
 const historicoRoutes = require('./routes/historico.routes'); // Gerenciamento de histórico
-const vitimaRoutes = require('./routes/vitima.routes');
+const vitimaRoutes = require('./routes/vitima.routes'); // Gerenciamento de vítimas
 
+// Configuração das rotas
 app.use('/api', authRoutes); // /api/login
 app.use('/api', protectedRoutes); // /api/protegido
-app.use('/api/users', userRoutes); // /api/usuarios
+app.use('/api/users', userRoutes); // /api/users
 app.use('/api/casos', caseRoutes); // /api/casos
 app.use('/api/evidencias', evidenceRoutes); // /api/evidencias
 app.use('/api/laudos', laudoRoutes); // /api/laudos
-app.use('/api/relatorios', relatorioRoutes); // /api/relatorio
+app.use('/api/relatorios', relatorioRoutes); // /api/relatorios
 app.use('/api/historico', historicoRoutes); // /api/historico
-app.use('/api/vitimas', vitimaRoutes);
+app.use('/api/vitimas', vitimaRoutes); // /api/vitimas
 
 // Rota de teste
 app.get('/', (req, res) => {
