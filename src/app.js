d21bfa0a801022cee2ffad71fcaf6c2194d4d5da
Amanda Alves <165097403+AmandapasBB@users.noreply.github.com>
2025-06-10 02:59:17 +0000

    Resolução do erro nas rotas do swagger

--- src/app.js
+++ src/app.js
@@ -1,42 +1,42 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const { swaggerSpec } = require('../swagger');
 const app = express();
-const { swaggerUi, specs } = require('../swagger'); // ou './swagger' se estiver dentro de src
-
 
 // Configuração do MongoDB
 const uri = "mongodb+srv://amandapriscilaa15:S99rV4gzO6u9MYaw@cluster0.sd5eblw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 mongoose
-  .connect(uri, {
-    //useNewUrlParser: true,
-    //useUnifiedTopology: true,
-  })
-  .then(() => console.log('Conectado ao MongoDB Atlas'))
-  .catch(err => console.error('Erro ao conectar ao MongoDB Atlas:', err));
+ .connect(uri, {
+   //useNewUrlParser: true,
+   //useUnifiedTopology: true,
+ })
+ .then(() => console.log('Conectado ao MongoDB Atlas'))
+ .catch(err => console.error('Erro ao conectar ao MongoDB Atlas:', err));
 
 // Middleware para habilitar o CORS
 app.use(
-  cors({
-    origin: [
-      'http://127.0.0.1:5500', 
-      'http://localhost:3000',
-      'http://localhost:5173',
-      'https://sistema-olin.netlify.app',
-      'https://opulent-capybara-5gw9g7p49q734xwj-5173.app.github.dev'
-    ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-  })
+ cors({
+   origin: [
+     'http://127.0.0.1:5500', 
+     'http://localhost:3000',
+     'http://localhost:5173',
+     'https://sistema-olin.netlify.app',
+     'https://opulent-capybara-5gw9g7p49q734xwj-5173.app.github.dev'
+   ],
+   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+   allowedHeaders: ['Content-Type', 'Authorization'],
+   credentials: true
+ })
 );
 
 // Middleware para parsing de JSON
 app.use(express.json());
 
+// Swagger Documentation
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
 
 // Importa a função createAdmin para executá-la ao iniciar
 require('./create/createAdmin');
@@ -69,10 +69,9 @@ app.use('/api/laudos-odontologicos', laudoOdontologicoRoutes);
 app.use('/api/analytics', analyticsRoutes);
 app.use('/api/marcacoes', marcacaoRoutes);
 
-
 // Rota de teste
 app.get('/', (req, res) => {
-  res.json({ message: 'API de Gerenciamento de Casos Periciais está funcionando!' });
+ res.json({ message: 'API de Gerenciamento de Casos Periciais está funcionando!' });
 });
 
 module.exports = app;
